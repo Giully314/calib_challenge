31df4cc34b88aa0a0743f85c78dc11f3678d15fc
Giully314 <giully@protonmail.com>
2021-06-26 01:29:53 +0200

    training functions

---
+++ src/training.py
@@ -0,0 +1,58 @@
+import torch
+import numpy as np
+import time 
+
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    loss = loss_func(model(xb), yb)
+    
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        
+    return loss.item(), len(xb)
+
+
+def fit(epochs, model, loss_func, opt, scheduler, train_dls, valid_dls, dev) -> dict:
+    """
+    Return a dict with 2 keys: train_loss and val_loss.
+    Each element of the list correspond to the loss of the respective epoch idx.
+    """
+
+    history = {"train_loss": [], "val_loss": []}
+    total_time = 0
+    for epoch in range(epochs):
+        since = time.time()
+        print(f"Start epoch #{epoch}...")
+        
+        model.train()
+        train_loss = 0        
+        for train_dl in train_dls:
+            losses, nums = zip(*[loss_batch(model, loss_func, xb.to(dev), yb.to(dev), opt) for xb, yb in train_dl])
+            train_loss += np.sum(np.multiply(losses, nums)) / np.sum(nums)
+                                        
+        history["train_loss"].append(train_loss)
+
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():      
+            for valid_dl in valid_dls: 
+                model.zero_hidden()   
+                losses, nums = zip(*[loss_batch(model, loss_func, xb.to(dev), yb.to(dev)) for xb, yb in valid_dl])
+                val_loss += np.sum(np.multiply(losses, nums)) / np.sum(nums)
+
+
+        if scheduler is not None:
+            scheduler.step(val_loss)
+
+
+        history["val_loss"].append(val_loss)
+
+        time_elapsed = time.time() - since
+        total_time += time_elapsed
+        print(f"Train_loss: {train_loss}")
+        print(f"Val_loss: {val_loss}")
+        print(f'Epoch complete in {(time_elapsed // 60):.0f}m {(time_elapsed % 60):.0f}s\n')
+
+    print(f"Total time for training {(total_time//60):.0f}m {(total_time % 60):.0f}s")
+    return history
