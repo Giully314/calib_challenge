a56bb6c15c158c07a3811062482e06714d78d59f
Giully314 <giully@protonmail.com>
2021-07-20 23:54:49 +0200

    Adding the timer for every step of the conversion

--- src/conversion.py
+++ src/conversion.py
@@ -1,11 +1,12 @@
 import argparse
-from torch.utils import data
 from torchvision import transforms as T
 from torchvision.transforms import InterpolationMode
 import os
 import frames_preprocessing as fp
+from custom_transform import bgr_to_rgb, Crop
+import utils as ut
 
-#TODO add time and verbose mode
+#TODO add time and verbose mode 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--cpus", type=int, default=2, help="Number of cores to use.")
@@ -17,7 +18,7 @@ parser.add_argument("-h_div", "--height_divisor", type=float, metavar="H", defau
 parser.add_argument("-w_div", "--width_divisor", type=float, metavar="W", default=3, help="Divisor of the width.")
 parser.add_argument("--train_split", type=float, metavar="train_split", default=0.8, help="A number between 0 and 1 for the train split.")
 parser.add_argument("--test_split", type=float, metavar="test_split", default=0.1, help="A number between 0 and 1 for the test split.")
-parser.add_argument("-m", "--merge", type=bool, metavar="merge", default=False, help="Merge the frames in one directory.")
+parser.add_argument("-m", "--merge", action="store_true", help="Merge the frames in one directory.")
 parser.add_argument("-j", "--jitter", type=float, metavar="b c s h", nargs="*", help="Specify color jitter. See pytorch doc.")
 parser.add_argument("-t", "--translate", type=float, metavar="th tw", nargs="*", help="Specify image translation. See pytorch doc.")
 parser.add_argument("-r", "--rotation", type=float, metavar="a1 a2", nargs="*", help="Specify image rotation. See pytorch doc.")
@@ -28,7 +29,7 @@ args = parser.parse_args()
 
 # print(args)
 
-conversion_type = args.conversion_type
+#conversion_type = args.conversion_type
 
 num_of_cpu = args.cpus
 merge = args.merge
@@ -36,15 +37,15 @@ HEIGHT = 874
 WIDTH = 1164
 height_div = args.height_divisor
 width_div = args.width_divisor
-new_height = HEIGHT // height_div
-new_width = WIDTH // width_div
+new_height = int(HEIGHT // height_div)
+new_width = int(WIDTH // width_div)
 
 train_split = args.train_split
 test_split = args.test_split
 
-
+# print(new_height, new_width)
 trf_resize = T.Resize((new_height, new_width), interpolation=InterpolationMode.BICUBIC)
-basic_transform = T.Compose([fp.bgr_to_rgb, T.ToTensor(), trf_resize])
+basic_transform = T.Compose([bgr_to_rgb, T.ToTensor(), trf_resize])
 
 
 trf_normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], inplace=True)
@@ -84,7 +85,7 @@ crop_x1 = args.crop[0]
 crop_x2 = args.crop[1]
 crop_y1 = args.crop[2]
 crop_y2 = args.crop[3]
-trf_crop = fp.Crop(crop_x1, crop_x2, crop_y1, crop_y2)
+trf_crop = Crop(crop_x1, crop_x2, crop_y1, crop_y2)
 
 
 trf_standard = T.Compose([trf_crop, trf_normalize])
@@ -109,71 +110,109 @@ if trf_translate is not None:
 #Basic conversion
 video_names = args.videos
 data_dir = args.output
-fp.create_dir(data_dir)
+ut.create_dir(data_dir)
 video_dir = args.input
 
 videos = [os.path.join(video_dir, str(video_name) + ".hevc") for video_name in video_names]
 angles = [os.path.join(video_dir, str(video_name) + ".txt") for video_name in video_names]
 outputs = [os.path.join(data_dir, str(video_name)) for video_name in video_names]
-fp.create_dirs(outputs)
+ut.create_dirs(outputs)
 
+timer = ut.Timer()
+
+#Setup videos 
+print("Start setup videos.")
+timer.start()
 fp.setup_videos(videos, outputs, angles, basic_transform, num_of_cpu=num_of_cpu)
+timer.end()
+print(f"Finished setup video in {timer}")
 
 
 #Split dataset
+print("Start split data.")
+timer.start()
 fp.split_train_valid_test(outputs, data_dir, train_split, test_split, num_of_cpu=num_of_cpu)
+timer.end()
+print("Finished split data in {timer}.")
 
 
 #augment data
 basic_train_dir = os.path.join(data_dir, "basic_train")
 inputs = [os.path.join(basic_train_dir, video_name) for video_name in video_names]
 train_dir = os.path.join(data_dir, "train")
-fp.create_dir(train_dir)
+ut.create_dir(train_dir)
 
+print("Start augment frames with standard transform.")
+timer.start()
 count = 0
 standard_dirs = [os.path.join(train_dir, str(video_name)) for video_name in video_names]
-fp.create_dirs(standard_dirs)
+ut.create_dirs(standard_dirs)
 fp.augment_videos(inputs, standard_dirs, trf_standard, num_of_cpu=num_of_cpu)
 count += len(standard_dirs)
+timer.end()
+print("Finished augment frames with standard transform in {timer}.")
+
 
 merge_dirs = [*standard_dirs] #for merge
 
 if trf_jitter is not None:
+    print("Start augment frames with jitter transform.")
+    timer.start()
     jitter_dirs = [os.path.join(train_dir, str(video_name + count)) for video_name in video_names]
     merge_dirs += jitter_dirs
-    fp.create_dirs(jitter_dirs)
+    ut.create_dirs(jitter_dirs)
     fp.augment_videos(inputs, jitter_dirs, trf_jitter, num_of_cpu=num_of_cpu)
     count += len(jitter_dirs)
+    timer.end()
+    print("Finished augment frames with jitter transform in {timer}.")
 
 if trf_rotation is not None:
+    print("Start augment frames with rotation transform.")
+    timer.start()
     rotation_dirs = [os.path.join(train_dir, str(video_name + count)) for video_name in video_names]
     merge_dirs += rotation_dirs
-    fp.create_dirs(rotation_dirs)
+    ut.create_dirs(rotation_dirs)
     fp.augment_videos(inputs, rotation_dirs, trf_rotation, num_of_cpu=num_of_cpu)
     count += len(rotation_dirs)
+    timer.end()
+    print("Finished augment frames with rotation transform in {timer}.")
 
 if trf_translate is not None:
+    print("Start augment frames with translate transform.")
+    timer.start()
     translate_dirs = [os.path.join(train_dir, str(video_name + count)) for video_name in video_names]
     merge_dirs += translate_dirs
-    fp.create_dirs(translate_dirs)
+    ut.create_dirs(translate_dirs)
     fp.augment_videos(inputs, translate_dirs, trf_translate, num_of_cpu=num_of_cpu)
     count += len(translate_dirs)
+    timer.end()
+    print("Finished augment frames with translate transform in {timer}.")
 
 
 #normalize validation and test set
+print("Start normalize validation set.")
+timer.start()
 valid_dir = os.path.join(data_dir, "valid")
 valid_dirs = [os.path.join(valid_dir, d) for d in os.listdir(valid_dir) if os.path.isdir(d)]
 fp.augment_videos(valid_dirs, valid_dirs, trf_standard, num_of_cpu=num_of_cpu)
+timer.end()
+print("Finished normalize validation set in {timer}")
 
+print("Start normalize test set.")
+timer.start()
 test_dir = os.path.join(data_dir, "test")
 test_dirs = [os.path.join(test_dir, d) for d in os.listdir(test_dir) if os.path.isdir(d)]
 fp.augment_videos(test_dirs, test_dirs, trf_standard, num_of_cpu=num_of_cpu)
-
+timer.end()
+print("Finished normalize test set in {timer}")
 
 if merge:
+    print("Start merge frames.")
+    timer.start()
     #TODO remove empty directories.
     fp.merge_frames(merge_dirs, os.path.join(data_dir, "train_merged_frames"))
     fp.merge_frames(valid_dirs, os.path.join(data_dir), "valid_merged_frames")
     fp.merge_frames(test_dirs, os.path.join(data_dir), "test_merged_frames")
-
+    timer.end()
+    print("Finished merge frames in {timer}.")
 
