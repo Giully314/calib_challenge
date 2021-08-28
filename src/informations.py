af70915a9f9aa4348a394303f4ed8e2fd70af378
Giully314 <giully@protonmail.com>
2021-08-28 17:58:48 +0200

    Tensors to cpu in GradientFlowVisualization

--- src/informations.py
+++ src/informations.py
@@ -207,21 +207,21 @@ class GradientFlowVisualization:
         self.fig = None
 
     def register_gradient_flow(self, named_parameters):
-        def plot_grad_flow(named_parameters):
-            ave_grads = []
-            layers = []
-            for n, p in named_parameters:
-                if(p.requires_grad) and ("bias" not in n):
-                    layers.append(n)
-                    ave_grads.append(p.grad.abs().mean())
-            plt.plot(ave_grads, alpha=0.3, color="b")
-            plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
-            plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
-            plt.xlim(xmin=0, xmax=len(ave_grads))
-            plt.xlabel("Layers")
-            plt.ylabel("average gradient")
-            plt.title("Gradient flow")
-            plt.grid(True)
+        # def plot_grad_flow(named_parameters):
+        #     ave_grads = []
+        #     layers = []
+        #     for n, p in named_parameters:
+        #         if(p.requires_grad) and ("bias" not in n):
+        #             layers.append(n)
+        #             ave_grads.append(p.grad.abs().mean())
+        #     plt.plot(ave_grads, alpha=0.3, color="b")
+        #     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+        #     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+        #     plt.xlim(xmin=0, xmax=len(ave_grads))
+        #     plt.xlabel("Layers")
+        #     plt.ylabel("average gradient")
+        #     plt.title("Gradient flow")
+        #     plt.grid(True)
             
         if self.fig is None:
             self.fig = plt.figure(figsize=(20, 18), dpi=160)
@@ -232,8 +232,8 @@ class GradientFlowVisualization:
         for n, p in named_parameters:
             if(p.requires_grad) and ("bias" not in n):
                 layers.append(n)
-                ave_grads.append(p.grad.abs().mean())
-                max_grads.append(p.grad.abs().max())
+                ave_grads.append(p.grad.abs().mean().cpu())
+                max_grads.append(p.grad.abs().max().cpu())
         
         plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
         plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
