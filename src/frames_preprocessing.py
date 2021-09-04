95d3ed08f9aa13287e75e77da0f726a5ef0798f3
Giully314 <giully@protonmail.com>
2021-09-04 12:05:25 +0200

    Addition of saving single frames as tensors + convert test video

--- src/frames_preprocessing.py
+++ src/frames_preprocessing.py
@@ -3,6 +3,7 @@ from torchvision import transforms as T
 import cv2 as cv
 import os
 import numpy as np
+import utils as ut
 
 
 def from_video_to_tensors(video_path: str, transform: T.Compose) -> torch.Tensor:
@@ -47,5 +48,34 @@ def setup_videos(video_paths: list[str], output_paths: list[str], angles_paths: 
 
 
 
-
+def setup_videos_single_tensor(video_paths: list[str], output_paths: list[str], angles_paths: list[str], selected_frames_paths: list[str],
+                 transform: T.Compose) -> None:
+    """
+    Convert every video in video_paths and pair with the corresponding angles. Save the tensors in output_paths.
+    """
+    for video_path, output_path, angles_path, selected_frames_path in zip(video_paths, output_paths, angles_paths, selected_frames_paths):
+        print(f"Start conversion of {video_path}.")
         
+        frames = from_video_to_tensors(video_path, transform)
+        angles = np.loadtxt(angles_path, dtype=np.float64)
+        selected_frames = np.reshape(np.loadtxt(selected_frames_path, dtype=np.int32), (-1, 2))
+        for i in range(selected_frames.shape[0]):
+            k, j = selected_frames[i]
+            angles_without_nan = np.invert(np.isnan(angles[k:j, 0])) 
+            tensors = frames[k : j][angles_without_nan]
+            p = os.path.join(output_path, str(i))
+            ut.create_dir(p)
+            for u in range(tensors.size(0)):
+                torch.save(tensors[u].clone(), os.path.join(p, str(u) + ".pt"))
+
+            np.savetxt(os.path.join(p, "angles.txt"), angles[k:j][angles_without_nan])
+    
+
+def convert_test_video(video_path, output, angles_path, transform):
+    print(f"Convert test video {video_path}")
+
+    frames = from_video_to_tensors(video_path, transform)
+    angles = np.loadtxt(angles_path, dtype=np.float64)
+    
+    torch.save(frames, os.path.join(output, "test_video.pt"))
+    np.savetxt(os.path.join(output, "angles.txt"), angles)
